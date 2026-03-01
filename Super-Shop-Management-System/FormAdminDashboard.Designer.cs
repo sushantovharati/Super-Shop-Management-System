a64da478149f287d73963ba548c05e7dd0b31704
Sushanto Vharati <sushantovharati.sv@gmail.com>
2026-03-01 22:54:16 +0600

    Login and AdminDashboard UI add

---
+++ Super-Shop-Management-System/FormAdminDashboard.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Super_Shop_Management_System
+{
+    partial class FormAdminDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnInvoiceDashboard = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnUpdatePassword = new System.Windows.Forms.Button();
+            this.btnManageProduct = new System.Windows.Forms.Button();
+            this.btnManageEmployee = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(163)))), ((int)(((byte)(139)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnInvoiceDashboard);
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Controls.Add(this.btnUpdatePassword);
+            this.panel1.Controls.Add(this.btnManageProduct);
+            this.panel1.Controls.Add(this.btnManageEmployee);
+            this.panel1.Controls.Add(this.lblWelcome);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1282, 753);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnInvoiceDashboard
+            // 
+            this.btnInvoiceDashboard.BackColor = System.Drawing.Color.Honeydew;
+            this.btnInvoiceDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInvoiceDashboard.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInvoiceDashboard.Location = new System.Drawing.Point(402, 350);
+            this.btnInvoiceDashboard.Name = "btnInvoiceDashboard";
+            this.btnInvoiceDashboard.Size = new System.Drawing.Size(448, 57);
+            this.btnInvoiceDashboard.TabIndex = 5;
+            this.btnInvoiceDashboard.Text = "Invoice Dashboard";
+            this.btnInvoiceDashboard.UseVisualStyleBackColor = false;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.Honeydew;
+            this.btnLogout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogout.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Location = new System.Drawing.Point(402, 526);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(448, 57);
+            this.btnLogout.TabIndex = 4;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdatePassword
+            // 
+            this.btnUpdatePassword.BackColor = System.Drawing.Color.Honeydew;
+            this.btnUpdatePassword.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpdatePassword.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdatePassword.Location = new System.Drawing.Point(402, 438);
+            this.btnUpdatePassword.Name = "btnUpdatePassword";
+            this.btnUpdatePassword.Size = new System.Drawing.Size(448, 57);
+            this.btnUpdatePassword.TabIndex = 3;
+            this.btnUpdatePassword.Text = "Update Password";
+            this.btnUpdatePassword.UseVisualStyleBackColor = false;
+            // 
+            // btnManageProduct
+            // 
+            this.btnManageProduct.BackColor = System.Drawing.Color.Honeydew;
+            this.btnManageProduct.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnManageProduct.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManageProduct.Location = new System.Drawing.Point(402, 262);
+            this.btnManageProduct.Name = "btnManageProduct";
+            this.btnManageProduct.Size = new System.Drawing.Size(448, 57);
+            this.btnManageProduct.TabIndex = 2;
+            this.btnManageProduct.Text = "Manage Product ";
+            this.btnManageProduct.UseVisualStyleBackColor = false;
+            // 
+            // btnManageEmployee
+            // 
+            this.btnManageEmployee.BackColor = System.Drawing.Color.Honeydew;
+            this.btnManageEmployee.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnManageEmployee.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManageEmployee.Location = new System.Drawing.Point(402, 174);
+            this.btnManageEmployee.Name = "btnManageEmployee";
+            this.btnManageEmployee.Size = new System.Drawing.Size(448, 57);
+            this.btnManageEmployee.TabIndex = 1;
+            this.btnManageEmployee.Text = "Manage Employee";
+            this.btnManageEmployee.UseVisualStyleBackColor = false;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Cambria", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(306, 81);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(246, 59);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Welcome ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(558, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 59);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Admin_Name";
+            // 
+            // FormAdminDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 753);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormAdminDashboard";
+            this.Text = "AdminDashboard";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnInvoiceDashboard;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnUpdatePassword;
+        private System.Windows.Forms.Button btnManageProduct;
+        private System.Windows.Forms.Button btnManageEmployee;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label label1;
+    }
+}
--- Super-Shop-Management-System/FormLogin.Designer.cs
+++ Super-Shop-Management-System/FormLogin.Designer.cs
@@ -28,20 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.ckbPassword = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(227)))), ((int)(((byte)(202)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.ckbPassword);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.txtUserId);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblLogin);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1282, 753);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(113)))), ((int)(((byte)(111)))));
+            this.panel3.Location = new System.Drawing.Point(0, 47);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(504, 14);
+            this.panel3.TabIndex = 13;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(309, 445);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(111, 36);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(83, 393);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(337, 37);
+            this.txtPassword.TabIndex = 10;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // ckbPassword
+            // 
+            this.ckbPassword.AutoSize = true;
+            this.ckbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbPassword.Location = new System.Drawing.Point(83, 452);
+            this.ckbPassword.Name = "ckbPassword";
+            this.ckbPassword.Size = new System.Drawing.Size(151, 24);
+            this.ckbPassword.TabIndex = 9;
+            this.ckbPassword.Text = "Show Password";
+            this.ckbPassword.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 338);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 40);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 40);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "User ID";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(83, 492);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(337, 52);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserId.Location = new System.Drawing.Point(80, 286);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(340, 37);
+            this.txtUserId.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightCoral;
+            this.label2.Location = new System.Drawing.Point(74, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(346, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please Fill Up The Form Below To Login";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Cambria", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(66, 92);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(363, 79);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "User Login";
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(502, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(780, 753);
+            this.panel2.TabIndex = 14;
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1282, 753);
+            this.Controls.Add(this.panel1);
             this.Name = "FormLogin";
             this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.CheckBox ckbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLogin;
     }
 }
 
